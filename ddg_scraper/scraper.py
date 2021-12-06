56b78d912feb158747d872cdc4be69fb9e6bf530
Myxi <myxi@duck.com>
2021-12-06 16:05:17 +0530

    Bug fix and code improvements

--- ddg_scraper/scraper.py
+++ ddg_scraper/scraper.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import httpx
 from selectolax.parser import HTMLParser
 from yarl import URL
@@ -7,6 +9,23 @@ from ._dataclasses import Result
 
 def anext(async_iterator):
     return async_iterator.__anext__()
+
+
+def iter_over_async(ait, loop):
+    ait = ait.__aiter__()
+
+    async def get_next():
+        try:
+            obj = await ait.__anext__()
+            return False, obj
+        except StopAsyncIteration:
+            return True, None
+
+    while True:
+        done, obj = loop.run_until_complete(get_next())
+        if done:
+            break
+        yield obj
 
 
 def search(query: str):
@@ -21,7 +40,7 @@ async def asearch(query: str):
     """
     Searches :param:`query`
     """
-    generator = ResultAsyncGenerator(Search(query)._aiter_results())
+    generator = ResultAsyncGenerator(await Search(query)._aiter_results())
     return generator
 
 
@@ -47,7 +66,7 @@ class Search:
             response = httpx.get(self.query_url.human_repr())
             return response.text
         except httpx.ConnectTimeout:
-            raise ValueError('No results.') from None
+            raise ValueError("No results.") from None
 
     async def _async_get_html(self):
         try:
@@ -55,35 +74,18 @@ class Search:
                 response = await client.get(self.query_url.human_repr())
                 return response.text
         except httpx.ConnectTimeout:
-            raise ValueError('No results.') from None
+            raise ValueError("No results.") from None
 
     def _iter_results(self):
+        loop = asyncio.get_event_loop()
         html = self._get_html()
-        parser = HTMLParser(html, True)
-        results = parser.css_first("div.results")
-
-        for result in results.css("div.result"):
-            try:
-                title = result.css_first("a.result__a")
-                title_text = title.text()
-                title_url = URL(title.attrs["href"]).query.get("uddg")
-                description = result.css_first("a.result__snippet").text()
-                icon_url = (
-                    URL(result.css_first("img.result__icon__img").attrs["src"])
-                    .with_scheme("https")
-                    .human_repr()
-                )
-            except Exception:
-                raise ValueError('No results.') from None
-            yield Result(
-                title=title_text,
-                description=description,
-                url=title_url,
-                icon_url=icon_url,
-            )
+        return iter_over_async(self.get_results(html), loop)
 
     async def _aiter_results(self):
         html = await self._async_get_html()
+        return self.get_results(html)
+
+    async def get_results(self, html):
         parser = HTMLParser(html, True)
         results = parser.css_first("div.results")
 
@@ -99,7 +101,7 @@ class Search:
                     .human_repr()
                 )
             except Exception:
-                raise ValueError('No results.') from None
+                raise ValueError("No results.") from None
             yield Result(
                 title=title_text,
                 description=description,
--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 from ddg_scraper import search, asearch
 import asyncio
 
-# for i in search('hello'):
-#     print(i)
+for i in search('hello'):
+    print(i)
 
 async def main():
     async for i in await asearch("hello"):
